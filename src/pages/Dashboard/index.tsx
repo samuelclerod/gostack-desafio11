b742ae05846c0b2135ddb04b0571b1e2ae154c6d
Samuel Rodrigues <samuelclerod@gmail.com>
2021-01-22 00:17:52 -0300

    finished Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -32,6 +32,7 @@ interface Food {
   id: number;
   name: string;
   description: string;
+  category: number;
   price: number;
   thumbnail_url: string;
   formattedPrice: string;
@@ -54,12 +55,41 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // Load Foods from API
+      // const { data } = await api.get('foods');
+      const { data } = await api.get('foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
+      let returnedFoods = data.map(
+        (food: Food): Food => {
+          return {
+            ...food,
+            formattedPrice: formatValue(food.price),
+          };
+        },
+      );
+
+      // if (searchValue.trim().length > 0) {
+      //   returnedFoods = returnedFoods.filter((food: Food) =>
+      //     food.name.includes(searchValue),
+      //   );
+      // }
+
+      // if (selectedCategory !== undefined) {
+      //   returnedFoods = returnedFoods.filter(
+      //     (food: Food) => food.category === selectedCategory,
+      //   );
+      // }
+
+      setFoods(returnedFoods);
     }
 
     loadFoods();
@@ -68,6 +98,8 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadCategories(): Promise<void> {
       // Load categories from API
+      const { data } = await api.get('categories');
+      setCategories(data);
     }
 
     loadCategories();
@@ -75,6 +107,7 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    setSelectedCategory(selectedCategory === id ? undefined : id);
   }
 
   return (
